3be10dc9a33b66d0faa517d7a2a0a3a4bf439ab7
Valeria Avila <valeria.avila17@gmail.com>
2020-08-31 03:08:14 -0700

    Add project files.

---
+++ CPW212-UnitTestStarterCode/SimpleMath.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPW212_UnitTestStarterCode
+{
+    /*****************************************************************************
+     * 
+     * YOU MAY REFACTOR THE PARAMETER NAMES BUT DO NOT CHANGE THE METHOD SIGNATURES
+     * 
+     * ***************************************************************************/
+    public static class SimpleMath
+    {
+        public static double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        public static double Subtract(double a, double b)
+        {
+            return a - b;
+        }
+
+        public static double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+
+        public static double Divide(double a, double b)
+        {
+            if (b == 0 || b == -0)
+            {
+                throw new ArgumentException($"{nameof(b)} must be a value other than zero");
+            }
+
+            return a / b;
+        }
+    }
+}
---
+++ CPW212-UnitTestStarterCodeTests/SimpleMathTests.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CPW212_UnitTestStarterCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPW212_UnitTestStarterCode.Tests
+{
+    [TestClass()]
+    public class SimpleMathTests
+    {
+        double result; 
+        
+        [TestMethod()]
+        [DataRow(5, 10)]
+        [DataRow(0, 100)]
+        [DataRow(-1, -10)]
+        [DataRow(0, -0)]
+        public void Add_TwoNumbers_ReturnsSum(double num1, double num2)
+        {
+            //Arrange 
+            result = num1 + num2;
+
+            //Act
+            double sum = SimpleMath.Add(num1, num2);
+
+            //Assert
+
+            Assert.AreEqual(result, sum);
+
+        }
+
+        [TestMethod]
+        [DataRow(1, 10)]
+        [DataRow(0, 100)]
+        [DataRow(-1, -1)]
+        [DataRow(0, -0)]
+        public void Multiply_TwoNumbers_ReturnsProduct(double num1, double num2)
+        {
+            result = num1 * num2;
+
+            double product = SimpleMath.Multiply(num1, num2);
+
+            Assert.AreEqual(result, product);
+
+
+        }
+
+        [TestMethod]
+        [DataRow(4, -0)]
+        [DataRow(7, 0)]
+        public void Divide_DenominatorZero_ThrowsArgumentException(double num1, double num2)
+        {
+            Assert.ThrowsException<ArgumentException> ( () => SimpleMath.Divide(num1, num2) );
+        }
+
+        [TestMethod]
+        [DataRow(1, 20)]
+        [DataRow(0, 10)]
+        [DataRow(-1, -10)]
+        public void Divide_TwoValidNumberss_ReturnsQuotient(double num1, double num2)
+        {
+           result = num1 / num2;
+
+            double quotient = SimpleMath.Divide(num1, num2);
+
+            Assert.AreEqual(result, quotient);
+        }
+
+        [TestMethod()]
+        [DataRow(1, 10)]
+        [DataRow(0, 10)]
+        [DataRow(-1, -1)]
+        public void Subtract_TwoNumbers_ReturnsDifference(double num1, double num2)
+        {
+            result = num1 - num2;
+
+            double difference = SimpleMath.Subtract(num1, num2);
+
+            Assert.AreEqual(result, difference);
+        }
+    }
+}
